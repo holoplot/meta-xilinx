ca0b6e6e1d684347e7035f9faf4300fe0606e2f9
Raju Kumar Pothuraju <rajukumar.pothuraju@amd.com>
2025-05-22 17:07:40 +0530

    generate-fw-sdt.sh: Remove vek280-pl-aie-vdu-fw recipe from generate script
    vek280 base design updated to ced based which has the aie only as
    overlay tcl.
    Removing the existing vek280-pl-aie-vdu-fw recipe and create new recipe
    with only aie once sdt artifactories updated.
    
    Signed-off-by: Raju Kumar Pothuraju <rajukumar.pothuraju@amd.com>
    Signed-off-by: Mark Hatle <mark.hatle@amd.com>

--- meta-xilinx-demos/scripts/generate-fw-sdt.sh
+++ meta-xilinx-demos/scripts/generate-fw-sdt.sh
@@ -4,7 +4,6 @@
 ### Machine                 Recipe
 #M# vck190-pl-bram-gpio-fw  recipes-firmware/vck190-pl-bram-gpio-fw/vck190-pl-bram-gpio-fw_1.0-2025.2.bb
 #M# vek280-pl-bram-gpio-fw  recipes-firmware/vek280-pl-bram-uart-gpio-fw/vek280-pl-bram-uart-gpio-fw_1.0-2025.2.bb
-#M# vek280-pl-aie-vdu-fw    recipes-firmware/vek280-pl-aie-vdu-fw/vek280-pl-aie-vdu-fw_1.0-2025.2.bb
 #M# zcu104-pl-vcu-fw        recipes-firmware/zcu104-pl-vcu-fw/zcu104-pl-vcu-fw_1.0-2025.2.bb
 #M# zcu111-pl-bram-gpio-fw  recipes-firmware/zcu111-pl-bram-gpio-fw/zcu111-pl-bram-gpio-fw_1.0-2025.2.bb
 
