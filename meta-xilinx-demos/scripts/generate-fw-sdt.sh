83005b22dddf8408e12c09e84a07929ecf96f9e4
Suvrat Chaturvedi <SuvratShankar.Chaturvedi@amd.com>
2025-07-28 12:19:58 +0530

    meta-xilinx-demos: generate-fw-sdt.sh: Added the zc702 machine for automation flow
    Signed-off-by: Suvrat Chaturvedi <SuvratShankar.Chaturvedi@amd.com>
    Signed-off-by: Mark Hatle <mark.hatle@amd.com>

--- meta-xilinx-demos/scripts/generate-fw-sdt.sh
+++ meta-xilinx-demos/scripts/generate-fw-sdt.sh
@@ -8,6 +8,7 @@
 #M# zcu111-pl-bram-gpio-fw  recipes-firmware/zcu111-pl-bram-gpio-fw/zcu111-pl-bram-gpio-fw_1.0-2025.2.bb
 #M# vek385-pl-bram-gpio-fw  recipes-firmware/vek385-pl-bram-gpio-fw/vek385-pl-bram-gpio-fw_1.0-2025.2.bb
 #M# zcu106-pl-bram-gpio-fw  recipes-firmware/zcu106-pl-bram-gpio-fw/zcu106-pl-bram-gpio-fw_1.0-2025.2.bb
+#M# zc702-pl-bram-gpio-fw   recipes-firmware/zc702-pl-bram-gpio-fw/zc702-pl-bram-gpio-fw_1.0-2025.2.bb
 
 this=$(realpath $0)
 
