holoplot/meta-xilinx

commit 5e905ea84da6e45b3e87631eecbbd4b16315943d

meta-xilinx-demos: add OpenAMP components
Add recipes for the deployment side of the OpenAMP firmware examples to
support the following boards:

	Versal Gen 1:
		- L20
		- V80
		- VCK190
		- VEK280
		- VMK180
		- VPK120
		- VRK160
		- VRK165

	Versal Gen 2:
		- VCK385

These recipes populate a common filesystem image with the board-specific
OpenAMP firmware examples under the /lib/firmware/xilinx/<BOARD>/rpu directory
structure.

Also, add an OpenAMP packagegroup for the above firwmare examples.

Signed-off-by: Trevor Woerner <trevor.woerner@amd.com>

Changes /lib/firmware to ${nonarch_base_libdir}/firmware for YP compatibility.

Signed-off-by: Mark Hatle <mark.hatle@amd.com>

meta-xilinx-demos/dynamic-layers/meta-openamp/recipes-core/packagegroups/packagegroup-openamp-fw-examples.bb
@@ -1,0 +1,35 @@
+DESCRIPTION = "OpenAMP firmware examples"
+
+REQUIRED_DISTRO_FEATURES = "openamp"
+
+inherit packagegroup
+
+# packages common to all
+OPENAMP_PACKAGES_FULL_INSTALL = ""
+
+# CortexA72-specific (i.e. Versal Gen1) packages
+OPENAMP_VERSAL_GEN1_FULL_INSTALL = " \
+    l20-openamp-fw-examples \
+    v80-openamp-fw-examples \
+    vck190-openamp-fw-examples \
+    vek280-openamp-fw-examples \
+    vmk180-openamp-fw-examples \
+    vpk120-openamp-fw-examples \
+    vrk160-openamp-fw-examples \
+    vrk165-openamp-fw-examples \
+    "
+
+# CortexA78-mali-specific (i.e. Versal Gen2) packages
+OPENAMP_VERSAL_GEN2_FULL_INSTALL = " \
+    vek385-openamp-fw-examples \
+    "
+
+
+OPENAMP_PACKAGES:append = " ${OPENAMP_PACKAGES_FULL_INSTALL}"
+OPENAMP_PACKAGES:append:versal = " ${OPENAMP_VERSAL_GEN1_FULL_INSTALL}"
+OPENAMP_PACKAGES:append:versal-2ve-2vm = " ${OPENAMP_VERSAL_GEN2_FULL_INSTALL}"
+
+RDEPENDS:${PN} = "${OPENAMP_PACKAGES}"
+
+PACKAGE_ARCH = "${MACHINE_ARCH}"
+INSANE_SKIP:${PN} += "arch"

meta-xilinx-demos/dynamic-layers/meta-openamp/recipes-example/openamp/l20-openamp-fw-examples_2025.2.bb
@@ -1,0 +1,8 @@
+SUMMARY = "OpenAMP firmware examples demo payload processing"
+DESCRIPTION = "OpenAMP firmware examples demo payload processing"
+LICENSE = "CLOSED"
+
+SRC_URI = "https://petalinux.xilinx.com/sswreleases/rel-v2025.2/edf_files/2025.2/08120419/versal-l20-sdt-seg_packagegroup-openamp-fw-examples_08120419.tar.gz"
+SRC_URI[sha256sum]="c9227c2eb9002dfb8e752ef1e89fa34e79080e8ababdf6757fc96ce1b8212d4b"
+
+require openamp-fw-example-versal.inc

meta-xilinx-demos/dynamic-layers/meta-openamp/recipes-example/openamp/openamp-fw-example-versal.inc
@@ -1,0 +1,66 @@
+# all recipes using this *inc need to define the following variables
+# the following variables have the specified defaults
+# (some of which can be overridden)
+#    OPENAMPFW_CORE: the core on which the demo is to run
+#        default: "r5"
+#    OPENAMPFW_SLOT: the core slot on which the demo is to run
+#        default: "0"
+#    OPENAMPFW_OS: the OS on which the demo runs
+#        default: "freertos" for versal gen1
+#                 "zephyr" for versal gen2
+#    OPENAMPFW_DEMOS: the name of the demos demo
+#        default: "rpmsg-echo matrix-multiply rpc-demo" for versal gen1
+#                 "rpmsg_service" for versal gen2
+
+S = "${WORKDIR}/files"
+
+COMPATIBLE_MACHINE = "^$"
+COMPATIBLE_MACHINE:versal = "${MACHINE}"
+COMPATIBLE_MACHINE:versal-2ve-2vm = "${MACHINE}"
+
+do_compile[noexec] = "1"
+
+OPENAMPFW_BASEDIR = "${nonarch_base_libdir}/firmware/xilinx/rpu"
+OPENAMPFW_BOARD = "${@d.getVar("BPN").split("-")[0]}"
+OPENAMPFW_CORE ?= "r5"
+OPENAMPFW_SLOT ?= "0"
+OPENAMPFW_OS ?= ""
+OPENAMPFW_OS:versal ?= "freertos"
+OPENAMPFW_OS:versal-2ve-2vm ?= "zephyr"
+OPENAMPFW_DEMOS ?= ""
+OPENAMPFW_DEMOS:versal ?= "rpmsg-echo matrix-multiply rpc-demo"
+OPENAMPFW_DEMOS:versal-2ve-2vm ?= "rpmsg_service"
+
+do_install() {
+    if [ -z "${OPENAMPFW_CORE}" ]; then
+        bberror "OPENAMPFW_CORE is not set"
+        exit 1
+    fi
+    if [ -z "${OPENAMPFW_SLOT}" ]; then
+        bberror "OPENAMPFW_SLOT is not set"
+        exit 1
+    fi
+    if [ -z "${OPENAMPFW_OS}" ]; then
+        bberror "OPENAMPFW_OS is not set"
+        exit 1
+    fi
+    if [ -z "${OPENAMPFW_DEMOS}" ]; then
+        bberror "OPENAMPFW_DEMOS is not set"
+        exit 1
+    fi
+
+    install -d ${D}${OPENAMPFW_BASEDIR}
+    install -Dm 0644 ${S}/shell.json ${D}${OPENAMPFW_BASEDIR}
+
+    for DEMO in ${OPENAMPFW_DEMOS}; do
+        FULL_NAME=${OPENAMPFW_BOARD}-${OPENAMPFW_CORE}-${OPENAMPFW_SLOT}-${DEMO}-${OPENAMPFW_OS}
+
+        install -d ${D}${OPENAMPFW_BASEDIR}/${FULL_NAME}/${FULL_NAME}_slot${OPENAMPFW_SLOT}
+        install -Dm 0644 ${S}/${OPENAMPFW_BOARD}-${OPENAMPFW_CORE}-${OPENAMPFW_SLOT}-${DEMO}.elf ${D}${OPENAMPFW_BASEDIR}/${FULL_NAME}/${FULL_NAME}_slot${OPENAMPFW_SLOT}/${FULL_NAME}.elf
+   done
+}
+
+PACKAGE_ARCH = "${MACHINE_ARCH}"
+INSANE_SKIP:${PN} += "arch usrmerge"
+
+FILES:${PN} += "${OPENAMPFW_BASEDIR}"

meta-xilinx-demos/dynamic-layers/meta-openamp/recipes-example/openamp/v80-openamp-fw-examples_2025.2.bb
@@ -1,0 +1,8 @@
+SUMMARY = "OpenAMP firmware examples demo payload processing"
+DESCRIPTION = "OpenAMP firmware examples demo payload processing"
+LICENSE = "CLOSED"
+
+SRC_URI = "https://petalinux.xilinx.com/sswreleases/rel-v2025.2/edf_files/2025.2/08120419/versal-v80-sdt-seg_packagegroup-openamp-fw-examples_08120419.tar.gz"
+SRC_URI[sha256sum]="c9227c2eb9002dfb8e752ef1e89fa34e79080e8ababdf6757fc96ce1b8212d4b"
+
+require openamp-fw-example-versal.inc

meta-xilinx-demos/dynamic-layers/meta-openamp/recipes-example/openamp/vck190-openamp-fw-examples_2025.2.bb
@@ -1,0 +1,8 @@
+SUMMARY = "OpenAMP firmware examples demo payload processing"
+DESCRIPTION = "OpenAMP firmware examples demo payload processing"
+LICENSE = "CLOSED"
+
+SRC_URI = "https://petalinux.xilinx.com/sswreleases/rel-v2025.2/edf_files/2025.2/08120419/versal-vck190-sdt-seg_packagegroup-openamp-fw-examples_08120419.tar.gz"
+SRC_URI[sha256sum]="c9227c2eb9002dfb8e752ef1e89fa34e79080e8ababdf6757fc96ce1b8212d4b"
+
+require openamp-fw-example-versal.inc

meta-xilinx-demos/dynamic-layers/meta-openamp/recipes-example/openamp/vek280-openamp-fw-examples_2025.2.bb
@@ -1,0 +1,8 @@
+SUMMARY = "OpenAMP firmware examples demo payload processing"
+DESCRIPTION = "OpenAMP firmware examples demo payload processing"
+LICENSE = "CLOSED"
+
+SRC_URI = "https://petalinux.xilinx.com/sswreleases/rel-v2025.2/edf_files/2025.2/08120419/versal-vek280-sdt-seg-ospi_packagegroup-openamp-fw-examples_08120419.tar.gz"
+SRC_URI[sha256sum]="c9227c2eb9002dfb8e752ef1e89fa34e79080e8ababdf6757fc96ce1b8212d4b"
+
+require openamp-fw-example-versal.inc

meta-xilinx-demos/dynamic-layers/meta-openamp/recipes-example/openamp/vek385-openamp-fw-examples_2025.2.bb
@@ -1,0 +1,8 @@
+SUMMARY = "OpenAMP firmware examples demo payload processing"
+DESCRIPTION = "OpenAMP firmware examples demo payload processing"
+LICENSE = "CLOSED"
+
+SRC_URI = "https://petalinux.xilinx.com/sswreleases/rel-v2025.2/edf_files/2025.2/08120419/versal-2ve-2vm-vek385-sdt-seg_packagegroup-openamp-fw-examples_08120419.tar.gz"
+SRC_URI[sha256sum]="c9227c2eb9002dfb8e752ef1e89fa34e79080e8ababdf6757fc96ce1b8212d4b"
+
+require openamp-fw-example-versal.inc

meta-xilinx-demos/dynamic-layers/meta-openamp/recipes-example/openamp/vmk180-openamp-fw-examples_2025.2.bb
@@ -1,0 +1,8 @@
+SUMMARY = "OpenAMP firmware examples demo payload processing"
+DESCRIPTION = "OpenAMP firmware examples demo payload processing"
+LICENSE = "CLOSED"
+
+SRC_URI = "https://petalinux.xilinx.com/sswreleases/rel-v2025.2/edf_files/2025.2/08120419/versal-vmk180-sdt-seg_packagegroup-openamp-fw-examples_08120419.tar.gz"
+SRC_URI[sha256sum]="c9227c2eb9002dfb8e752ef1e89fa34e79080e8ababdf6757fc96ce1b8212d4b"
+
+require openamp-fw-example-versal.inc

commit 8efd6456d3930e60a0fd37c0ec212c3771cc6d86

packagegroup-openamp-fw-examples: restore original
The intent of this packagegroup was to use it to generate the build-time side
of these OpenAMP examples. When it was moved from an internal-only Yocto
layer to an external one, it was modified to act as a packagegroup for the
deploy-time side by mistake. This patch restores the original intent.

Fixes: 5e905ea84da6 ("meta-xilinx-demos: add OpenAMP components")
Signed-off-by: Trevor Woerner <trevor.woerner@amd.com>
Signed-off-by: Mark Hatle <mark.hatle@amd.com>

meta-xilinx-demos/dynamic-layers/meta-openamp/recipes-core/packagegroups/packagegroup-openamp-fw-examples.bb
@@ -1,35 +1,14 @@
 DESCRIPTION = "OpenAMP firmware examples"
 
-REQUIRED_DISTRO_FEATURES = "openamp"
-
 PACKAGE_ARCH = "${MACHINE_ARCH}"
 
 inherit packagegroup
 
-# packages common to all
-OPENAMP_PACKAGES_FULL_INSTALL = ""
-
-# CortexA72-specific (i.e. Versal Gen1) packages
-OPENAMP_VERSAL_GEN1_FULL_INSTALL = " \
-    l20-openamp-fw-examples \
-    v80-openamp-fw-examples \
-    vck190-openamp-fw-examples \
-    vek280-openamp-fw-examples \
-    vmk180-openamp-fw-examples \
-    vpk120-openamp-fw-examples \
-    vrk160-openamp-fw-examples \
-    vrk165-openamp-fw-examples \
+OPENAMP_PACKAGES = " \
+    openamp-fw-echo-testd-freertos \
+    openamp-fw-mat-muld-freertos \
+    openamp-fw-rpc-demo-freertos \
     "
-
-# CortexA78-mali-specific (i.e. Versal Gen2) packages
-OPENAMP_VERSAL_GEN2_FULL_INSTALL = " \
-    vek385-openamp-fw-examples \
-    "
-
-
-OPENAMP_PACKAGES:append = " ${OPENAMP_PACKAGES_FULL_INSTALL}"
-OPENAMP_PACKAGES:append:versal = " ${OPENAMP_VERSAL_GEN1_FULL_INSTALL}"
-OPENAMP_PACKAGES:append:versal-2ve-2vm = " ${OPENAMP_VERSAL_GEN2_FULL_INSTALL}"
 
 RDEPENDS:${PN} = "${OPENAMP_PACKAGES}"
 