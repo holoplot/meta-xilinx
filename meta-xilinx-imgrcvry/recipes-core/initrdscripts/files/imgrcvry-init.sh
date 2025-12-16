21f37b3a18bc350cf059f82729c2749836ac0350
Parthiban Kanchipuram <Parthiban.Kanchipuram@amd.com>
2025-12-16 23:47:11 +0530

    imgrcvry-init.sh: improve DHCP handling
    - Changed _UDHCPC_ARGS from "-n" to "-n -t 5 -T 2" so udhcpc exits if no lease
      is obtained ,retrying up to 5 times with 2s intervals.
    - Explicitly bring up eth0 and eth1 before starting DHCP.
    - Add a bounded retry loop to wait for an ip address to be assigned
      before proceeding.
    - This aviods false "No IP Addr found" errors during recovery when the
      link is up but DHCP completes later.
    
    Signed-off-by: Parthiban Kanchipuram <Parthiban.Kanchipuram@amd.com>
    Message-ID: <20251216181711.4050726-1-Parthiban.Kanchipuram@amd.com>
    Signed-off-by: Mark Hatle <mark.hatle@amd.com>

--- meta-xilinx-imgrcvry/recipes-core/initrdscripts/files/imgrcvry-init.sh
+++ meta-xilinx-imgrcvry/recipes-core/initrdscripts/files/imgrcvry-init.sh
@@ -47,7 +47,10 @@ launch_shell() {
 
 _UDHCPC="/sbin/udhcpc"
 # -n to exit if no lease
-_UDHCPC_ARGS="-n"
+_UDHCPC_ARGS="-n -t 5 -T 2"
+
+ip link set eth0 up 2>/dev/null
+ip link set eth1 up 2>/dev/null
 
 run_udhcpc() {
 	_UDHCPC_INT="$1"
@@ -82,7 +85,19 @@ run_udhcpc "-i eth1"
 
 launch_imgrcvry
 
-ip_addr=$(ip addr | grep -w inet | xargs | cut -d ' ' -f 2 | cut -d '/' -f 1)
+MAX_TRIES=10
+ip_addr=""
+i=1
+while [ $i -le $MAX_TRIES ]; do
+	ip_addr=$(ip -4 addr show scope global | awk '/inet / {print $2}' | cut -d/ -f1 | head -n1)
+	if [ -n "$ip_addr" ]; then
+		 echo "udhcpc: link is up, IP assigned -> $ip_addr"
+		 break
+	fi
+	sleep 1
+	i=$((i+1))
+done
+
 shell_msg=""
 if [ ! -z $ip_addr ]; then
 	shell_msg=", Use $ip_addr:8080 to launch the Image Recovery web app."
