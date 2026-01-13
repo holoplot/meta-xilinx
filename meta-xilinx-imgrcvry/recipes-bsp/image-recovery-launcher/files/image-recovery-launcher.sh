07ef112356bd5bde216b0f5de858b62d2bf07c6a
Parthiban Kanchipuram <Parthiban.Kanchipuram@amd.com>
2026-01-13 10:30:55 +0530

    image-recovery-launcher.sh: Fix variable quoting
    - Quote all variable expansions and use 'if cd' pattern for safer
      directory change with proper error handling.
    - Replace '[ ! -z ${var} ]' with POSIX-compliant '[ -n "${var}" ]'
      throughout the script.
    
    Signed-off-by: Parthiban Kanchipuram <Parthiban.Kanchipuram@amd.com>
    Message-Id: <20260113050055.2535175-1-Parthiban.Kanchipuram@amd.com>
    Signed-off-by: Sandeep Gundlupet Raju <sandeep.gundlupet-raju@amd.com>

--- meta-xilinx-imgrcvry/recipes-bsp/image-recovery-launcher/files/image-recovery-launcher.sh
+++ meta-xilinx-imgrcvry/recipes-bsp/image-recovery-launcher/files/image-recovery-launcher.sh
@@ -5,10 +5,13 @@ _IMGRCVRY_WEB="/var/imgrcry_web/"
 _IMGRCVRY="/usr/sbin/httpd"
 _IMGRCVRY_ARGS="-p 8080"
 
-if [ -x $_IMGRCVRY ]; then
-	cd $_IMGRCVRY_WEB
-	$_IMGRCVRY $_IMGRCVRY_ARGS
-	cd -
+if [ -x "$_IMGRCVRY" ]; then
+	if cd "$_IMGRCVRY_WEB"; then
+		"$_IMGRCVRY" $_IMGRCVRY_ARGS
+		cd - >/dev/null
+	else
+		echo "Warning: $_IMGRCVRY_WEB directory not found"
+	fi
 fi
 
 # Image recovery IP read and display banner
@@ -16,8 +19,8 @@ _IP_UTIL="/usr/sbin/ip"
 MAX_TRIES=10
 ip_addr=""
 tries=1
-while [ ${tries} -le $MAX_TRIES ]; do
-	ip_addr=$(${_IP_UTIL} -4 addr show scope global | awk '/inet / {print $2}' | cut -d/ -f1 | head -n1)
+while [ "${tries}" -le "$MAX_TRIES" ]; do
+	ip_addr=$("${_IP_UTIL}" -4 addr show scope global | awk '/inet / {print $2}' | cut -d/ -f1 | head -n1)
 	if [ -n "${ip_addr}" ]; then
 		echo "udhcpc: link is up, IP assigned -> ${ip_addr}"
 		break
@@ -29,7 +32,7 @@ while [ ${tries} -le $MAX_TRIES ]; do
 done
 
 shell_msg=""
-if [ ! -z ${ip_addr} ]; then
+if [ -n "${ip_addr}" ]; then
 	shell_msg=", Use ${ip_addr}:8080 to launch the Image Recovery web app."
 else
 	shell_msg=", No IP Addr found to launch the Image Recovery web app.   "
